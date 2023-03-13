67fa68e8c38f1e2355229c14aefb2a8b25bfa8e3
Shessire <125847387+Shessire@users.noreply.github.com>
2023-03-13 09:28:19 +0000

    assignment

---
+++ script.js
@@ -0,0 +1,25 @@
+const addNum = (a, b) => {
+    return (a + b);
+}
+addNum(2, 4);
+
+const subNum = (a, b) => {
+    return (a - b);
+}
+subNum(2, 4);
+
+const multiNum = (a, b) => {
+    return (a * b);
+}
+multiNum(2, 4);
+
+const divNum = (a, b) => {
+    return (a / b);
+}
+divNum(2, 4);
+
+const aboutMe = (name,location,hobby) => {
+    console.log(`Hi, my name is ${name}. I live in ${location} and enjoy ${hobby}`);
+}
+
+aboutMe('Pond', 'Thailand', 'playing video games');
